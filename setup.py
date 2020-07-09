17368fd4bc1630712fb84f5dc41331976e45041f
abulojoshua1 <abulojoshua1@gmail.com>
2020-07-09 09:38:45 +0300

    Remove software licence

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="oca", # Replace with your own username
+    name="oca",
     version="0.0.1",
     author="oca",
     author_email="info@geo-solutions.it",
@@ -15,10 +15,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/geosolutions-it/resiliencemada_oca",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
 )
